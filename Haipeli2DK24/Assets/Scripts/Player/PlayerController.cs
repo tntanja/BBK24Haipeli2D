7d24b2991757b2303874a9406e3f3d93a0cf6ca3
tntanja <tanjaa_88@hotmail.com>
2024-02-06 19:38:08 +0200

    6.2.24 tunnin jälkeen!

--- Haipeli2DK24/Assets/Scripts/Player/PlayerController.cs
+++ Haipeli2DK24/Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D body;
 
+    private Master controls;
+    private Vector2 moveInput;
+
     private void Awake() {
+        controls = new Master();
         body = GetComponent<Rigidbody2D>();
+    }
+
+    // kun objekti on aktiivinen
+    private void OnEnable() {
+        controls.Enable();      // aktivoi input controllsin
+    }
+
+    // kun objekti ei ole aktiivinen
+    private void OnDisable() {
+        controls.Disable();     // pysäyttää input controllsin
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move() {
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(body.position + movement);
     }
 }
