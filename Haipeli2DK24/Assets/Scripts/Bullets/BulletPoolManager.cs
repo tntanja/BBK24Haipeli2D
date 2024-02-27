08b3c718f05f385427e48c720932493beddd304c
tntanja <tanjaa_88@hotmail.com>
2024-02-27 19:38:24 +0200

    27.2.24 tunnin jälkeen laitettu

--- Haipeli2DK24/Assets/Scripts/Bullets/BulletPoolManager.cs
+++ Haipeli2DK24/Assets/Scripts/Bullets/BulletPoolManager.cs
@@ -11,14 +11,31 @@ public class BulletPoolManager : MonoBehaviour
     private Queue<GameObject> bulletPool = new Queue<GameObject>();
     
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        Instance = this;
+        InitializePool();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void InitializePool() {
+        for (int i = 0; i < poolSize; i++){
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.SetActive(false);
+            bulletPool.Enqueue(newBullet);
+        }
     }
+
+    public GameObject GetBullet() {
+        if(bulletPool.Count > 0){
+            GameObject bullet = bulletPool.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        } else {
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.SetActive(true);
+            return newBullet;
+        }
+    }
+
+    
 }
